71c55f6fb9e57f742fdb563d60f25c4b4fcf1cf1
Leonardo Degl'Innocenti <leonardo.deglinnocenti@stud.unifi.it>
2023-06-02 19:37:32 +0200

    Ver 2.3.1: Hotfix: SECRET_KEY has been secured.

--- makeYourChoice/settings.py
+++ makeYourChoice/settings.py
@@ -21,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bvvgys435^o+@s0bt27y=22*7i%e3ygy9hu4f13m(bk@%bz@dn'
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = 'django-insecure-bvvgys435^o+@s0bt27y=22*7i%e3ygy9hu4f13m(bk@%bz@dn'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
